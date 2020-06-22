1541116832efe49b73f594dbc2e1d16ca61655e5
kappafon <goran.rihter@gmail.com>
2020-06-22 07:51:53 +0200

    Updates

---
+++ precache-manifest.48836dc28de01bd52987ed697a519a5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df2a259e5d6a7c2db9d5316f7f82b4ca",
+    "url": "/fcc-d3-scatter-plot/index.html"
+  },
+  {
+    "revision": "d9aedca4521d45cc16fa",
+    "url": "/fcc-d3-scatter-plot/static/css/main.9f6e4d72.chunk.css"
+  },
+  {
+    "revision": "fba489176905da14df24",
+    "url": "/fcc-d3-scatter-plot/static/js/2.49f34088.chunk.js"
+  },
+  {
+    "revision": "d9aedca4521d45cc16fa",
+    "url": "/fcc-d3-scatter-plot/static/js/main.e39a8652.chunk.js"
+  },
+  {
+    "revision": "47d2413f579028c1e5d9",
+    "url": "/fcc-d3-scatter-plot/static/js/runtime-main.5f54307a.js"
+  }
+]);
